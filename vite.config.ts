0135562ddc2989948b660e8a561b66ed0d8df517
Viridian <565769755@qq.com>
2023-09-11 21:08:40 +0800

    feat(init): 初始化前端仓库

---
+++ vite.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import { resolve } from 'path';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+    },
+  },
+  server: {
+    port: 8520,
+    open: true,
+    host: true,
+    proxy: {
+      '/api': {
+        target: '',
+        changeOrigin: true,
+      },
+    },
+  },
+})
